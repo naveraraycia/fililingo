9d82b09a5b8d926b557b57fc49bae081ae8895ad
naveraraycia <naveraraycia@yahoo.com>
2023-07-10 15:56:40 -0700

    Added propTypes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function Card({children}) {
   return (
     <div className="card w-96 bg-neutral text-base-content">
@@ -6,6 +8,10 @@ function Card({children}) {
       </div>
     </div>
   )
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default Card
--- src/components/TextGroup.jsx
+++ src/components/TextGroup.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function TextGroup({subText, mainText}) {
   return (
     <div className='text-center space-y-[-15px]'>
@@ -5,6 +7,11 @@ function TextGroup({subText, mainText}) {
       <div className="text-7xl leading-[110px] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#367051] to-[#E3BC52] md:leading-snug md:text-8xl">{mainText}</div>
     </div>
   )
+}
+
+TextGroup.propTypes = {
+  subText: PropTypes.string.isRequired,
+  mainText: PropTypes.string.isRequired
 }
 
 export default TextGroup
